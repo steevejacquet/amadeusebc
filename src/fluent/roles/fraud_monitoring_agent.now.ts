2d729fc19b623038588fdc7a10e97d89bf64cbad
Edwin Coronado <edwin.coronado@servicenow.com>
2025-10-16 09:15:16 -0700

    added a new comment

--- src/fluent/roles/fraud_monitoring_agent.now.ts
+++ src/fluent/roles/fraud_monitoring_agent.now.ts
@@ -1,6 +1,7 @@
 import '@servicenow/sdk/global';
 import { Role } from '@servicenow/sdk/core';
 
+//TODO: rename this role
 export const fraud_monitoring_agent = Role({
   name: 'x_snc_reservatio_2.fraud_monitoring_agent',
   description: 'Role for agents who monitor and investigate fraud alerts in the reservation system'
